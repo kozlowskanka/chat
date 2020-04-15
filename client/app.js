b075b9fcaed5431a364049ca5437b7d1cf446c35
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-15 21:10:38 +0200

    Set sendMessage

--- client/app.js
+++ client/app.js
@@ -11,6 +11,10 @@ loginForm.addEventListener('submit', function(event) {
     login(event);
 });
 
+addMessageForm.addEventListener('submit', function(event) {
+    sendMessage(event);
+});
+
 const login = event => {
     event.preventDefault();
     if(userNameInput.value == '') {
@@ -22,3 +26,26 @@ const login = event => {
     }
 };
 
+const sendMessage = event => {
+    event.preventDefault();
+    if(messageContentInput.value == '') {
+        alert('type your message ;)');
+    } else {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    }
+};
+
+const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+};
